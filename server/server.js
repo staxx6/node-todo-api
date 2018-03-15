2259f22e68f0beecc242bc06380d8a6e71e9d7df
staxx6 <staxx@hotmail.de>
2018-03-15 22:24:18 +0100

    update

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+const port = 8000;
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(port, () => {
+    console.log('Startet on port', port);
+});
